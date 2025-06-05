277a2a06d9b6bf737d8b81676a98c77c01f0afb2
TheBank <mr.mark.novak@gmail.com>
2025-06-04 18:44:52 -0700

    More prep work

--- experiment.py
+++ experiment.py
@@ -1,9 +1,23 @@
 from utils import CreateCOAssessedEvent
 from utils import CreatePredicateAssessedEvent
+from utils import CreatePredicateFailedEvent, CreatePredicateSucceededEvent
 from utils import DeploymentStream, Application, Environment
+from utils import CcmClient
+from esdbclient import StreamState
 
 #evidence = "some data"
 #print(CreateCOAssessedEvent(False, "CCV", 2, evidence))
 #print(CreatePredicateAssessedEvent(True, "CCV", 1, 1))
 
-print(DeploymentStream(Application.Payroll, Environment.DEV))
+#print(DeploymentStream(Application.Payroll, Environment.DEV))
+
+predicateFailedEvent = CreatePredicateFailedEvent("CCV", 1, 1, 12, "Expired Certificate")
+predicateSucceededEvent = CreatePredicateSucceededEvent("RP", 2, 2)
+
+PayrollDeploymentStream = DeploymentStream(Application.Payroll, Environment.DEV)
+client = CcmClient()
+client.append_to_stream(
+    PayrollDeploymentStream,
+    events=[predicateFailedEvent, predicateSucceededEvent],
+    current_version=StreamState.ANY
+)
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ DefaultPort = "2113"
 def KurrentUri(host = DefaultHost, port = DefaultPort):
     return "esdb://" + host + ":" + port + "?tls=false"
 
-def Client(host = DefaultHost, port = DefaultPort):
+def CcmClient(host = DefaultHost, port = DefaultPort):
     return EventStoreDBClient(KurrentUri(host, port))
 
 def DeploymentStream(suffix):
@@ -21,7 +21,7 @@ def DeploymentStream(suffix):
 #
 
 class Application(Enum):
-    Payroll = 1234
+    Payroll = 1236
     Timecard = 5678
 
 class Environment(Enum):
@@ -33,27 +33,37 @@ def DeploymentStream(app: Application, env: Environment) -> str:
     return app.name + "-" + str(app.value) + "-" + env.name
 
 #
-# Event Types
+# Control Procedure Helpers
 #
 
-# Control Objective Event Types
-COPrefix = "CO-"
-COAchieved = "ControlObjectiveAchieved"
-COFailed = "ControlObjectiveFailed"
+CPPrefix = "CP-"
+def CPIdentifier(CPID: int) -> str:
+    return CPPrefix + str(CPID)
+
+#
+# Control Objective Helpers
+#
 
 # Control Objective Event Creation
+COPrefix = "CO-"
 def COIdentifier(CODomain: str, COID: int) -> str:
     return COPrefix + CODomain + "-" + str(COID)
 
+# Control Objective Event Types
+COAchieved = "ControlObjectiveAchieved"
+COFailed = "ControlObjectiveFailed"
+
 def CreateCOAssessedEvent(success: bool, CODomain: str, COID: int, evidence: str) -> NewEvent:
     dataString = \
-        "{\"id\":\"" + COIdentifier(CODomain, COID) + "\"," + \
-        "\"evidence\":\"" + evidence + "\"" + \
-        "}"
+        "{\"ControlObjectiveID\":\"" + COIdentifier(CODomain, COID) + "\"," + \
+        "\"Evidence\":\"" + evidence + "\"}"
     return NewEvent(
         type = COAchieved if success else COFailed,
-        data = dataString.encode('utf-8')
-    )
+        data = dataString.encode('utf-8'))
+
+#
+# Predicate Helpers
+#
 
 # Predicate Event Types
 PredicateSucceeded = "PredicateSucceeded"
@@ -62,12 +72,24 @@ PredicateFailed = "PredicateFailed"
 def PredicateIdentifier(CODomain: str, COID: int, PredicateID: int) -> str:
     return CODomain + "-" + str(COID) + "-" + str(PredicateID)
 
-# Predict Event Creation
-def CreatePredicateAssessedEvent(success: bool, CODomain: str, COID: str, PredicateID: int) -> NewEvent:
+# Predicate Event Creation
+def CreatePredicateAssessedEvent(success: bool, CODomain: str, COID: str, PredicateID: int, evidence: str = "\"\"") -> NewEvent:
     dataString = \
-        "{\"id\":\"" + PredicateIdentifier(CODomain, COID, PredicateID) + "\"" + \
-        "}"
+        "{\"PredicateID\":\"" + PredicateIdentifier(CODomain, COID, PredicateID) + "\"," + \
+        "\"Evidence\":" + evidence + "}"
     return NewEvent(
         type = PredicateSucceeded if success else PredicateFailed,
-        data = dataString.encode('utf-8')
-    )
+        data = dataString.encode('utf-8'))
+
+FailedCP = "FailedControlProcedure"
+
+def CreatePredicateSucceededEvent(CODomain: str, COID: int, PredicateID: int) -> NewEvent:
+    return CreatePredicateAssessedEvent(True, CODomain, COID, PredicateID)
+
+def ReasonForCPFailure(CPID: int, reason: str) -> str:
+    return \
+        "{\"" + FailedCP + "\":\"" + CPIdentifier(CPID) + "\"," + \
+        "\"Reason\":\"" + reason + "\"}"
+
+def CreatePredicateFailedEvent(CODomain: str, COID: int, PredicateID: int, CPID: int, reason: str) -> NewEvent:
+    return CreatePredicateAssessedEvent(False, CODomain, COID, PredicateID, ReasonForCPFailure(CPID, reason))
