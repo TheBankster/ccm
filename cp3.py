9301392b74253394777d31e30fb3c6ef5e7341bc
TheBank <mr.mark.novak@gmail.com>
2025-06-07 07:47:03 -0700

    Removed stale tests

--- cp3.py
+++ cp3.py
@@ -38,11 +38,3 @@ class TEEState(ControlProcedureState):
 class TEEIsolation(ControlProcedure):
     def __init__(self, stream: str, owner: str, state: TEEState):
         ControlProcedure.__init__(self, ControlProcedureId, stream, owner, state)
-
-test1 = TEEState(correctCode=True, correctConfiguration=True)
-test2 = TEEState(correctCode=False, correctConfiguration=True)
-test3 = TEEState(correctCode=True,correctConfiguration=False)
-result2 = test1.Validate(test2)
-result3 = test1.Validate(test3)
-print(result2.success, result2.evidence)
-print(result3.success, result3.evidence)
